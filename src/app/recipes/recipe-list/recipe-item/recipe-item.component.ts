5dbf386ca42b787d5e1af7ab1a168f3d38b3dd01
Arturo Navarro <arturonavarro@outlook.com>
2020-03-12 00:04:14 +0100

    se agregan directivas

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.scss']
 })
-export class RecipeItemComponent implements OnInit {
+export class RecipeItemComponent {
+  @Input()
+  recipe: Recipe;
+  @Output()
+  onSelectRecipe= new EventEmitter<void>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  onRecipleClick(event: Event) {
+    event.preventDefault();
+    this.onSelectRecipe.emit();
   }
-
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  @Output()
+  onSelectRecipe = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is a simply recipe', 'https://static01.nyt.com/images/2019/12/11/dining/11as-pasta-with-ricotta-and-lemon/merlin_155855151_a42a72df-8230-420d-b9b9-d81fe089ff55-articleLarge.jpg')
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipeClick(recipe: Recipe) {
+    this.onSelectRecipe.emit(recipe);
+  }
 }
